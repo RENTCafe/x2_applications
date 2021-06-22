072ce21072134924bfda3696f2b1f3820f60a1c5
RENTCafe <107330539+RENTCafe@users.noreply.github.com>
2021-06-22 17:58:55 +0000

    chore: bump package version

--- app/src/main/java/com/jaychang/charts/DemoActivity.kt
+++ app/src/main/java/com/jaychang/charts/DemoActivity.kt
@@ -3,4 +3,5 @@ package com.jaychang.charts
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DemoActivity : Ap
+class DemoActivity : AppCompatActivity() {
+    overr
