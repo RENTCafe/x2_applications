1ac914d1d180507f714c7e1f4a893639cec600b0
RENTCafe <107330539+RENTCafe@users.noreply.github.com>
2021-07-04 15:12:11 +0000

    Debug cleanup

--- app/src/main/java/com/jaychang/charts/StaticData.kt
+++ app/src/main/java/com/jaychang/charts/StaticData.kt
@@ -11,3 +11,24 @@ fun barDataList(): List<BarData> {
             high = 180.99f,
             low = 178.57f,
             close = 179.85f
+        ),
+        BarData(
+            time = LocalDate.parse("2018-10-22"),
+            open = 180.82f,
+            high = 181.40f,
+            low = 177.56f,
+            close = 178.75f
+        ),
+        BarData(
+            time = LocalDate.parse("2018-10-23"),
+            open = 175.77f,
+            high = 179.49f,
+            low = 175.44f,
+            close = 178.53f
+        ),
+        BarData(
+            time = LocalDate.parse("2018-10-24"),
+            open = 178.58f,
+            high = 182.37f,
+            low = 176.31f,
+            close = 176.97f
